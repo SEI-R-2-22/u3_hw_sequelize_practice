59903933126c2da2b142106dc5285cf3bf0d8b8c
Huan <huan.zeng1@gmail.com>
2022-04-09 21:56:46 -0400

    added models

---
+++ migrations/20220410015237-create-shopping-cart.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('shopping_carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_id: {
+        type: Sequelize.BIGINT
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('shopping_carts');
+  }
+};

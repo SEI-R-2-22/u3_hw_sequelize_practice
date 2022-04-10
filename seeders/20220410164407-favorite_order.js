f8a60ff297ac21a1bee5016d2a288a799f50f6e7
Job85 <julianjernigan@ymail.com>
2022-04-10 19:01:51 -0400

    seeded favorite_orders

--- seeders/20220410164407-favorite_order.js
+++ seeders/20220410164407-favorite_order.js
@@ -13,34 +13,34 @@ module.exports = {
     */
     await queryInterface.bulkInsert('favorite_orders', [{
       item_id: 5,
-      user_id: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      item_id: 7,
-      user_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      item_id: 8,
-      user_id: 4,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      item_id: 2,
-      user_id: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      item_id: 4,
       user_id: 6,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      item_id: 7,
-      user_id: 5,
+      item_id: 10,
+      user_id: 6,
       createdAt: new Date(),
       updatedAt: new Date()
+      // }, {
+      //   item_id: 4,
+      //   user_id: 4,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }, {
+      //   item_id: 7,
+      //   user_id: 4,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }, {
+      //   item_id: 3,
+      //   user_id: 5,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }, {
+      //   item_id: 8,
+      //   user_id: 1,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
     }])
   },
 

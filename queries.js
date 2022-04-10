ae57e06bb59c51f6b11ea03c2ae020fa02d361a8
Huan <huan.zeng1@gmail.com>
2022-04-09 23:56:01 -0400

    finish all CRUD fn

--- queries.js
+++ queries.js
@@ -1,5 +1,123 @@
-const { User, Product, Review } = require('./models')
+const { User, Product, Review, sequelize } = require('./models')
+const review = require('./models/review')
 
 const stringify = (data) => {
   console.log(JSON.stringify(data, null, 2))
+}
+
+/***************************/
+/*************User**************/
+/***************************/
+
+const createUser = async () => {
+  const res = await User.create({
+    userName: 'NewUser',
+    password: 'random123',
+    email: 'test@test.com'
+  })
+  stringify(res)
+}
+
+const readUser = async () => {
+  const res = await User.findAll()
+
+  stringify(res)
+}
+
+const updateUser = async () => {
+  const res = await User.update(
+    {
+      email: 'random@test.com',
+      password: 'NotRandom123'
+    },
+    {
+      where: {
+        userName: 'NewUser'
+      }
+    }
+  )
+  console.log('update successful')
+}
+
+const deleteUser = async () => {
+  const res = await User.destroy({ where: { userName: 'NewUser' } })
+}
+
+/***************************/
+/*************Product**************/
+/***************************/
+
+const createProduct = () => {
+  const res = await Product.create({
+    name: "newProduct",
+    price: 999,
+    description: 'Aswesome new stuff'
+  })
+  stringify(res)
+}
+const readProduct = () => {
+  const res = await Product.findAll()
+  stringify(res)
+}
+const updateProduct = () => {
+  const res = await Product.update({
+    price: 9.99,
+    description: 'ALL much go'
+  },{where: {name: 'newProduct'}})
+  console.log('update sucessful')
+}
+const deleteProduct = () => {
+  const res = await Product.destroy({where: {name: 'newProduct'}})
+}
+
+/***************************/
+/*************Review**************/
+/***************************/
+
+const createReview = () => {
+  const res = await Review.create(
+    {
+      comments: 'this is an aswesome product',
+      rating: 5,
+    }
+  )
+  stringify(res)
+}
+const readReview = () => {
+  const res = await Review.findAll()
+  stringify(res)
+}
+const updateReview = () => {
+  const res = await Review.update({
+    rating: 4,
+    commments: 'product not working after one day'
+  },{where: {comments: 'this is an aswesome product'}})
+  console.log('update successful')
+}
+
+const deleteReview = () => {
+  const res = await Review.destroy({where: {rating: 4}})
+}
+
+const main = async () => {
+  try {
+    // await readUser()
+    // await createUser()
+    // await updateUser()
+    // await deleteUser()
+
+    // await createProduct()
+    // await readProduct()
+    // await updateProduct()
+    // await deleteProduct()
+
+    // await createReview()
+    // await readReview()
+    // await updateReview()
+    // await deleteReview()
+  } catch (error) {
+    console.log(error)
+  }finally{
+    await sequelize.close()
+  }
 }

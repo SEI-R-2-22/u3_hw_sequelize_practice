2b31f1b8ec55ed603163ffa93f136d5230b1a084
Job85 <julianjernigan@ymail.com>
2022-04-11 01:03:23 -0400

    added CRUD to userQueries.js

---
+++ userQueries.js
@@ -0,0 +1,48 @@
+// const { Sequelize } = require('sequelize')
+// const { FavoriteOrder, sequelize } = require('./models/favorite_order')
+// const { Item, sequelize } = require('./models/item')
+// const { Menu, sequelize } = require('./models/menu')
+const { User, sequelize } = require('./models')
+const { Op } = require('sequelize')
+
+const stringify = (data) => {
+    console.log(JSON.stringify(data, null, 2))
+}
+
+async function main() {
+    try {
+        //find all users
+        // const users = await User.findAll()
+        // stringify(users)
+
+        //create user
+        // const jules = await User.create({ first_name: 'Jules', last_name: 'Verne', password: 'JourneyCenterOfEarth', favorite_order_id: 0 });
+        // console.log("Jules' auto-generated ID:", jules.id);
+        // const user = await User.create({
+        //     first_name: 'Captain',
+        //     last_name: 'Nemo',
+        //     password: '20000Leagues',
+        //     favorite_order_id: 0,
+        // });
+        // console.log(user.first_name)
+
+        //update user
+        // await User.update({ favorite_order_id: 2 }, {
+        //     where: {
+        //         first_name: 'Pasquale'
+        //     }
+        // });
+
+        //delete user
+        // await User.destroy({
+        //     where: {
+        //         last_name: null
+        //     }
+        // });
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await sequelize.close()
+    }
+}
+main()
